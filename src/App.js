99b880a9f212f71a0e6f983e32b675adb6209f62
Emily <bruner.emilym@gmail.com>
2019-12-11 22:16:59 -0500

    added nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 
 function App() {
   return (
     <div>
+      <Header />
+      <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
