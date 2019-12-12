eeb3d3e3d60cd13a58881298b8089ede2e734610
Emily <bruner.emilym@gmail.com>
2019-12-11 22:00:53 -0500

    created collection preview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview';
 
 class ShopPage extends React.Component {
     constructor (props) {
@@ -11,8 +12,16 @@ class ShopPage extends React.Component {
     }
 
     render() {
+        const {collections} = this.state;
         return (
-            <div>SHOP PAGE</div>
+            <div className='shop-page'>
+                {
+                    collections.map(({id, ...otherCollectionProps}) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
+            </div>
+            
         )
     }
 }
