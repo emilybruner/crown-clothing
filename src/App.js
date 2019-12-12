24b2b43f7dcfcc49087a45776da6977e32094470
Emily <bruner.emilym@gmail.com>
2019-12-11 21:10:12 -0500

    started routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import {Route} from 'react-router-dom';
+import HatsPage from './pages/hatspage/hatspage.component';
 
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
---
+++ src/pages/hatspage/hatspage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HatsPage = () => {
+    return (
+        <div>
+            <h1>Hats Page</h1>
+        </div>
+    )
+}
+
+export default HatsPage;
