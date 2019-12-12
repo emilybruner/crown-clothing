0640d49c83275be662520b722dc163a55c059ae6
Emily <bruner.emilym@gmail.com>
2019-12-11 23:10:52 -0500

    styled sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import FormInput from '../../components/form-input/form-input.component';
-
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -36,7 +36,7 @@ class SignIn extends React.Component{
                     <FormInput name="password" type="password" value={this.state.email} handleChange={this.handleChange} label="password"required />
                     
 
-                    <input type="submit" value="Submit Form" />
+                    <CustomButton type="submit" value="Submit Form"> SIGN IN </CustomButton>
                 </form>
             </div>
         )
