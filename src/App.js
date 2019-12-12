cae5a98127d624c7051c0bec3b1e5a078acb31e5
Emily <bruner.emilym@gmail.com>
2019-12-11 21:18:02 -0500

    added some data

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 import {Route} from 'react-router-dom';
-import HatsPage from './pages/hatspage/hatspage.component';
+
 
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+  
     </div>
   );
 }
