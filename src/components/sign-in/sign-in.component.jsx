8355dd3f006ecdc6e7d7f45d64230c1f6d3e8f90
Emily <bruner.emilym@gmail.com>
2019-12-13 22:35:54 -0500

    finished sign in handlesubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -15,10 +15,19 @@ class SignIn extends React.Component{
         }
     }
 
-   handleSubmit = event => {
+   handleSubmit = async event => {
        event.preventDefault();
 
-       this.setState({email: '', password: ''})
+       const {email, password} = this.state;
+
+       try {
+           await auth.signInWithEmailAndPassword(email, password);
+           this.setState({email: '', password: ''})
+       } catch (error) {
+           console.log(error)
+       }
+
+       
    }
 
     handleChange = event => {
@@ -35,7 +44,7 @@ class SignIn extends React.Component{
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email" required />
-                    <FormInput name="password" type="password" value={this.state.email} handleChange={this.handleChange} label="password"required />
+                    <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password"required />
                     
                     <div className='buttons'>
                     <CustomButton type="submit" value="Submit Form"> SIGN IN </CustomButton>
