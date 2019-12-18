4affaf0bd371d49c69a7c751c1db4c0d5016a5b9
Emily <bruner.emilym@gmail.com>
2019-12-17 22:00:11 -0500

    set up redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../assests/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 
@@ -23,4 +24,8 @@ const Header = ({currentUser}) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
