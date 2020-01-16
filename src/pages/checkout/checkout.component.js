85830b999a81ff6f2bbb872005690ea6ae00301d
Emily <bruner.emilym@gmail.com>
2020-01-16 15:41:42 -0500

    added checkout page, checkout item component

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
 
@@ -26,10 +27,9 @@ const CheckoutPage = ({cartItems, total}) => (
            </div>
        </div>
        {
-           cartItems.map(cartItem => 
-            cartItem.name
-        )
-       }
+           cartItems.map(cartItem => (
+            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+           ))}
        <div className='total'>
            <span>TOTAL: ${total}</span>
        </div>
